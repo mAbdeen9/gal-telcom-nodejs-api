88d5f584011c6818983518d0c7595bf94d05a23d
mAbdeen9 <93052095+mAbdeen9@users.noreply.github.com>
2022-09-09 13:30:33 +0300

    decoding token sync

--- Controller/authController.js
+++ Controller/authController.js
@@ -12,8 +12,8 @@ exports.protect = catchAsync(async (req, res, next) => {
   if (!token) {
     return next(new AppError('אתה לא מחובר', 401));
   }
-
-  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  const decoded = jwt.verify(token,  process.env.JWT_SECRET);
+  // const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
   //  Check if user still exists
   const currentUser = await User.findById(decoded.id);
